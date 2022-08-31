2c37b8436036b4b4fcbc97c076a3a6cc804f9491
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-08-31 13:58:55 -0400

    Add files via upload

---
+++ PillBox/demo/demo/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../src/Types.h"
+
+using namespace Pillbox;
+
+
+int main(int argc, const char* argv[]) {
+    std::cout << "Hello, World!\n";
+    Engine engine;
+    engine.StartUp();
+    return 0;
+}
