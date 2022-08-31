29a39210c91ae9c55fdd88386888dfa9faa286bb
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-08-31 17:13:27 -0400

    added game loop logic to Engine.cpp

--- PillBox/src/Engine.cpp
+++ PillBox/src/Engine.cpp
@@ -1,4 +1,6 @@
 #include "Engine.h"
+#include <thread>
+#include <chrono>
 
 
 using namespace Pillbox;
@@ -17,5 +19,24 @@ void Engine::StartUp()
 void Engine::ShutDown()
 {
 	graphics.ShutDown();
+}
+
+void Engine::RunGameLoop()
+{
+	graphics.StartUp();
+
+	const auto one_sixteith_of_a_second = std::chrono::duration<real>(1. / 60.);
+	while (true)
+	{
+		auto t1 = std::chrono::steady_clock::now(); //start of tick
+		graphics.Draw();
+
+		auto t2 = std::chrono::steady_clock::now(); //end of tick
+	
+		auto time_took = t2 - t1;
+		std::this_thread::sleep_for(one_sixteith_of_a_second - time_took);
+	}
+
+	ShutDown();
 }
 
