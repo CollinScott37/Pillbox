6367102e14b4dee032a0934dc4ca6b2416bad0ac
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-09-07 16:49:49 -0400

    Attempting to do callback

--- PillBox/demo/helloworld.cpp
+++ PillBox/demo/helloworld.cpp
@@ -2,12 +2,23 @@
 
 #include "../src/Types.h"
 #include "../src/Engine.h"
+#include "../src/EngineRef.h"
 using namespace Pillbox;
 
+char c = 'a';
 
 int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!\n";
-    Engine engine;
-    engine.StartUp();
+    /*
+    EngineRef e;
+    e.engine->RunGameLoop(&PrintInput);
+    */
+    Engine e;
+    e.RunGameLoop(&PrintInput);
     return 0;
+}
+
+void PrintInput(char c)
+{
+    std::cout << "Pressing: " << c << "\n";
 }
