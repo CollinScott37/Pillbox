56772c400f88a3695705b32e53f4695d8aac24df
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-09-09 16:01:26 -0400

    updated for checkpoint 3 and 4

--- PillBox/demo/helloworld.cpp
+++ PillBox/demo/helloworld.cpp
@@ -3,18 +3,59 @@
 #include "../src/Types.h"
 #include "../src/Engine.h"
 #include "../src/EngineRef.h"
+
 using namespace Pillbox;
 
-char c = 'a';
 
 int main(int argc, const char* argv[]) {
-    std::cout << "Hello, World!\n";
-    /*
-    EngineRef e;
-    e.engine->RunGameLoop(&PrintInput);
-    */
+    
+    std::cout << "Takes a min to load\n";
     Engine e;
-    e.RunGameLoop(&PrintInput);
+
+    bool prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
+    bool prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
+    bool prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
+    bool prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
+
+    std::cout << "done loading"
+    e.RunGameLoop([&]() {
+
+
+        
+        if (prevInputq == false && e.input.KeyIsPressed(GLFW_KEY_Q) == true)
+        {
+            std::cout << "Quack!\n";
+            e.sounds.PlaySoundKeyboard(GLFW_KEY_Q);
+        }
+        
+
+        if (prevInputn == false && e.input.KeyIsPressed(GLFW_KEY_N) == true)
+        {
+            std::cout << "Boom!\n";
+            e.sounds.PlaySoundKeyboard(GLFW_KEY_N);
+        }
+
+        if (prevInputp == false && e.input.KeyIsPressed(GLFW_KEY_P) == true)
+        {
+            std::cout << ";)\n";
+            e.sounds.PlaySoundKeyboard(GLFW_KEY_P);
+        }
+
+        if (prevInputr == false && e.input.KeyIsPressed(GLFW_KEY_R) == true)
+        {
+            std::cout << "uh oh\n";
+            e.sounds.PlaySoundKeyboard(GLFW_KEY_R);
+        }
+
+        prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
+        prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
+        prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
+        prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
+       
+        return;
+        
+        });
+
     return 0;
 }
 
