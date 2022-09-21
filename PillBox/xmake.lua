5c1808b2da2973abf5844947f03bace14f129994
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-09-21 12:06:09 -0400

    Add files via upload

--- PillBox/xmake.lua
+++ PillBox/xmake.lua
@@ -1,16 +1,28 @@
 set_policy("build.warning", true) -- show warnings
 set_warnings("all") -- warn about many things
-
+includes("external/xmake_soloud.lua")
 add_rules("mode.debug", "mode.release")
 add_requires("glfw")
+add_requires("soloud")
+add_requires("sokol")
+add_requires("glm")
+add_requires("stb")
 
 target("Pillbox")
 set_kind("static")
 set_languages("cxx17")
+    -- Copy assets
+    after_build(function (target)
+        cprint("Copying assets")
+        os.cp("$(projectdir)/assets", path.directory(target:targetfile()))
+    end)
 
 -- Adds packacge to project
-add_packages("glfw")
-
+add_packages("glfw", {public = true})
+add_packages("soloud", {public = true})
+add_packages("sokol", {public = true})
+add_packages("glm", {public = true})
+add_packages("stb", {public = true})
 -- Declare our engine's header path.
 -- This allows targets that depend on the engine to #include them.
 add_includedirs("src", {public = true})
