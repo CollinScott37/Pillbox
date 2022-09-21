db53a1cc02c1793f94a2009847d4e31f3c904e27
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-09-21 12:06:31 -0400

    Add files via upload

--- PillBox/demo/helloworld.cpp
+++ PillBox/demo/helloworld.cpp
@@ -2,7 +2,6 @@
 
 #include "../src/Types.h"
 #include "../src/Engine.h"
-#include "../src/EngineRef.h"
 
 using namespace Pillbox;
 
@@ -11,49 +10,44 @@ int main(int argc, const char* argv[]) {
     
     std::cout << "Takes a min to load\n";
     Engine e;
+    e.StartUp();
+    //e.sounds.LoadSound("quack.wav", "sounds");
+    //bool prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
+    //bool prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
+    //bool prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
+    //bool prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
 
-    bool prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
-    bool prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
-    bool prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
-    bool prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
-
-    std::cout << "done loading"
+    std::cout << "done loading";
     e.RunGameLoop([&]() {
-
-
         
-        if (prevInputq == false && e.input.KeyIsPressed(GLFW_KEY_Q) == true)
+        if (e.input.GetKeyCodeUp(GLFW_KEY_Q) == true)
         {
             std::cout << "Quack!\n";
-            e.sounds.PlaySoundKeyboard(GLFW_KEY_Q);
+            //e.sounds.PlaySound("quack.wav");
         }
-        
 
-        if (prevInputn == false && e.input.KeyIsPressed(GLFW_KEY_N) == true)
+
+        if (e.input.GetKeyCodeUp(GLFW_KEY_N) == true)
         {
             std::cout << "Boom!\n";
-            e.sounds.PlaySoundKeyboard(GLFW_KEY_N);
+            //e.sounds.PlaySoundKeyboard(GLFW_KEY_N);
         }
 
-        if (prevInputp == false && e.input.KeyIsPressed(GLFW_KEY_P) == true)
+        if (e.input.GetKeyCodeUp(GLFW_KEY_P) == true)
         {
             std::cout << ";)\n";
-            e.sounds.PlaySoundKeyboard(GLFW_KEY_P);
+            // e.sounds.PlaySoundKeyboard(GLFW_KEY_P);
         }
 
-        if (prevInputr == false && e.input.KeyIsPressed(GLFW_KEY_R) == true)
+        if (e.input.GetKeyCodeUp(GLFW_KEY_R) == true)
         {
             std::cout << "uh oh\n";
-            e.sounds.PlaySoundKeyboard(GLFW_KEY_R);
+            //e.sounds.PlaySoundKeyboard(GLFW_KEY_R);
         }
 
-        prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
-        prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
-        prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
-        prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
-       
+
         return;
-        
+
         });
 
     return 0;
