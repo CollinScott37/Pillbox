5a2abd18e74117696d414d872a49e30577f5c674
CollinScott37 <77978143+CollinScott37@users.noreply.github.com>
2022-09-22 15:56:00 -0400

    Add files via upload

--- PillBox/demo/helloworld.cpp
+++ PillBox/demo/helloworld.cpp
@@ -9,15 +9,21 @@ using namespace Pillbox;
 int main(int argc, const char* argv[]) {
     
     std::cout << "Takes a min to load\n";
+    
     Engine e;
     e.StartUp();
     e.sounds.LoadSound("quack.wav", "sounds");
-    //bool prevInputq = e.input.KeyIsPressed(GLFW_KEY_Q);
-    //bool prevInputn = e.input.KeyIsPressed(GLFW_KEY_N);
-    //bool prevInputp = e.input.KeyIsPressed(GLFW_KEY_P);
-    //bool prevInputr = e.input.KeyIsPressed(GLFW_KEY_R);
+    
+    //load both image and sprite
+    //e.graphics.LoadImageFile("gred.png", "images");
+    //e.graphics.LoadSprite("gred.png", vec2(0), vec3(10), 0.0);
 
-    std::cout << "done loading";
+
+    e.graphics.LoadImageFile("goose.png", "images");
+    e.graphics.LoadSprite("goose.png", vec2(0), vec3(50), 0.0);
+
+    std::cout << "done loading\n";
+
     e.RunGameLoop([&]() {
         
         if (e.input.GetKeyCodeUp(GLFW_KEY_Q) == true)
@@ -27,33 +33,9 @@ int main(int argc, const char* argv[]) {
         }
 
 
-        if (e.input.GetKeyCodeUp(GLFW_KEY_N) == true)
-        {
-            std::cout << "Boom!\n";
-            //e.sounds.PlaySoundKeyboard(GLFW_KEY_N);
-        }
-
-        if (e.input.GetKeyCodeUp(GLFW_KEY_P) == true)
-        {
-            std::cout << ";)\n";
-            // e.sounds.PlaySoundKeyboard(GLFW_KEY_P);
-        }
-
-        if (e.input.GetKeyCodeUp(GLFW_KEY_R) == true)
-        {
-            std::cout << "uh oh\n";
-            //e.sounds.PlaySoundKeyboard(GLFW_KEY_R);
-        }
-
-
         return;
 
         });
 
     return 0;
-}
-
-void PrintInput(char c)
-{
-    std::cout << "Pressing: " << c << "\n";
 }
